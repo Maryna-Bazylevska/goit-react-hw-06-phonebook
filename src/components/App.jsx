9a63b8270cf3961ce46cf6a2401e7e0a573f467b
Maryna-Bazylevska <83463971+Maryna-Bazylevska@users.noreply.github.com>
2022-04-22 16:43:58 +0200

    correction

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,27 @@
- import React, { useState , useEffect} from "react";
- import { nanoid } from "nanoid";
- import ContactForm from './ContactForm';
- import ContactList from "./ContactList";
- import Filter from './Filter';
- import Notification from './Notification';
- import {Wrapper} from './App.styles';
- import { useSelector } from "react-redux";
- 
+import React from "react";
+import ContactForm from "./ContactForm";
+import ContactList from "./ContactList";
+import Filter from "./Filter";
+import Notification from "./Notification";
+import { Wrapper } from "./App.styles";
+import { useSelector } from "react-redux";
+import { getContact } from "../redux/contactSelectors";
 function App() {
-const contacts = useSelector(state => state.contacts);
-
-
-
+  const contacts = useSelector(getContact);
   return (
     <Wrapper>
-       <h1>Phonebook</h1>
-       <ContactForm  />
-       {contacts.length < 1 ? (
+      <h1>Phonebook</h1>
+      <ContactForm />
+      {contacts.length < 1 ? (
         <Notification text="Contact list is empty" />
       ) : (
         <div>
-       <h2>Contacts</h2>
-       <Filter />
-       <ContactList/>
-       </div>
+          <h2>Contacts</h2>
+          <Filter />
+          <ContactList />
+        </div>
       )}
     </Wrapper>
-  ); 
+  );
 }
 export default App;
